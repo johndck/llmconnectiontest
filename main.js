397e097b29d4ac21a0abb22323cd1872a2f8c5f4
John Dick <johnturnerdick@gmail.com>
2024-07-16 21:49:29 +0100

    added in web scraping tests

--- main.js
+++ main.js
@@ -1,1 +1,12 @@
-console.log(`Hello, world!`);
+const puppeteer = require('puppeteer');
+
+(async () => {
+  const browser = await puppeteer.launch();
+  const page = await browser.newPage();
+  await page.goto('https://www.legalandgeneral.com/retirement/pensions/personal-pension/');
+  await page.screenshot({path: 'example.png'});
+
+  await browser.close();
+}   )();    
+
+
