322ef89617115013dae302704a0b90160231d6f9
John Dick <johnturnerdick@gmail.com>
2024-07-17 21:36:50 +0100

    added improved screenshot

--- main.js
+++ main.js
@@ -3,8 +3,23 @@ const puppeteer = require('puppeteer');
 (async () => {
   const browser = await puppeteer.launch();
   const page = await browser.newPage();
-  await page.goto('https://www.legalandgeneral.com/retirement/pensions/personal-pension/');
-  await page.screenshot({path: 'example.png'});
+
+await page.setViewport({
+    width: 1920,
+    height: 1080,
+    deviceScaleFactor: 1,
+  });
+
+  await page.goto('https://www.legalandgeneral.com/retirement/pensions/personal-pension/',{waitUntil: 'networkidle0', timeout:0});
+  await page.waitForSelector('#onetrust-accept-btn-handler', { visible: true });
+  await page.click('#onetrust-accept-btn-handler');
+
+  await page.evaluate(() => window.scrollTo(0, document.body.scrollHeight));
+
+  await page.waitForTimeout(3000);
+
+
+  await page.screenshot({path: 'example.png', fullPage: true});
 
   await browser.close();
 }   )();    
